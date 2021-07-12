8c812622a2d05a773b8f2a53ba73a22199018ad9
秋本 裕之 <hiroyuki.akimoto@willof.co.jp>
2021-07-12 09:17:38 +0900

    editへ情報を渡す箇所まで

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,11 @@
     <v-app>
         <Header />
         <v-main>
-            <router-view />
+            <v-container>
+                <router-view
+                    :books="books"
+                    @add-book-list="addBook" />
+            </v-container>
         </v-main>
         <Footer />
     </v-app>
@@ -11,6 +15,7 @@
 <script>
 import Header from '@/global/Header.vue'
 import Footer from '@/global/Footer.vue'
+const STORAGE_KEY = 'books'
 
 export default {
     name: "App",
@@ -20,8 +25,47 @@ export default {
     },
     data() {
         return{
-
+            books: [],
+            newBooks: null
         }
     },
+    mounted() {
+        if (localStorage.getItem(STORAGE_KEY)) {
+            try {
+                this.books = JSON.parse(localStorage.getItem(STORAGE_KEY));
+            } catch(e) {
+                localStorage.removeItem(STORAGE_KEY);
+            }
+        }
+    },
+    methods: {
+        addBook(e) {
+            this.books.push({
+                id: this.books.length,
+                title: e.title,
+                image: e.image,
+                discription: e.discription,
+                readDate: '',
+                mome: '',
+            });
+            // this.newBooks = '';
+            this.saveBooks();
+
+            // 最後に追加したIDを取得するコード
+            // console.log(this.books.slice(-1)[0].id)
+            this.goToEditPage(this.books.slice(-1)[0].id)
+        },
+        removeBook(x) {
+            this.books.splice(x, 1);
+            this.saveBooks();
+        },
+        saveBooks() {
+            const parsed = JSON.stringify(this.books);
+            localStorage.setItem(STORAGE_KEY, parsed);
+        },
+        goToEditPage(id) {
+            this.$router.push(`/edit/${id}`)
+        }
+    }
 };
 </script>
--- src/pages/BookEdit.vue
+++ src/pages/BookEdit.vue
@@ -1,11 +1,24 @@
 <template>
     <div>
-        ブックエディット
+        {{ books }}
     </div>
 </template>
 
 <script>
 export default {
+    name: "BookEdit",
+    props: {
+        books: Array
+    },
+    beforeRouteEnter (to, from, next) {
+        // vueはすべて非同期のため、処理を遅延させるコード
+        // next内ではthisが使えないためvmで代用
+        next( vm => {
+            vm.$nextTick( () => {
+                vm.book = vm.books[vm.$route.params.id]
+            })
+        })
+    }
 
 }
 </script>
